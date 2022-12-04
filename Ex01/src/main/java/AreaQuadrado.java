b4d0402eac3734e707cc9fd48882e6ab6a5f1929
Mateus Cadete <91164291+mateuscadete@users.noreply.github.com>
2022-12-04 20:27:32 -0300

    Exercício 01 - Linguagem de Programação
    Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.

---
+++ Ex01/src/main/java/AreaQuadrado.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+/*
+1. Coletar o valor de um quadrado, calcular sua área 
+e apresentar o resultado.
+ */
+
+public class AreaQuadrado {
+    public static void main(String Args[]){
+        double a = 0, resultado = 0;
+        
+        // Coleta o valor do lado do quadrado
+        a = Integer.parseInt(JOptionPane.showInputDialog("Digite o lado do quadrado: "));
+        
+        // Calcula a área do quadrado utilizando o lado elevado ao quadrado p/ calcular a área
+        resultado = a * a;
+        
+        // Mostra o resultado
+        System.out.println("O resultado é: " + resultado);
+    
+    }
+}

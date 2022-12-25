37d7b0e993f814758a9692c695fbcd1bde092e6b
Mateus Cadete <91164291+mateuscadete@users.noreply.github.com>
2022-12-25 13:35:29 -0300

    Exercício 12 - Linguagem de Programação
    Objetivo: Receba o raio de uma circunferência. Calcule e mostre o comprimento da
    circunferência.

---
+++ Ex12/src/main/java/mavenproject/ex1/Ex12.java
@@ -0,0 +1,31 @@
+package mavenproject.ex1;
+
+import javax.swing.JOptionPane;
+
+/*
+12. Receba o ano de nascimento e o ano atual. Calcule e mostre a sua idade e
+quantos anos terá daqui a 17 anos.
+ */
+
+public class Ex12 {
+
+    public static void main(String[] args) {
+        int anoNascimento = 0, anoAtual = 0, idade = 0;
+        
+        //Recebe o ano de nascimento e atribui à variável correspondente
+        anoNascimento = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano "
+            + "de nascimento: "));
+        
+        //Recebe o ano atual e atribui à variável correspondente
+        anoAtual = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano"
+            + " atual: "));
+        
+        /* Calcula a idade com base na variável anoNascimento e
+        anoAtual e atribui a variável idade */
+        idade = anoAtual - anoNascimento;
+        
+        // Mostra a idade
+        JOptionPane.showMessageDialog(null, "A idade é: " + idade);
+               
+    }
+}
